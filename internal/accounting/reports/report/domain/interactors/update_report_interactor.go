71f87950c57be6ffda2c6d03c62c1e01a5d7b922
zhuravlevma <monge.gaspard.1746@gmail.com>
2024-02-11 17:59:37 +0300

    fix: add update interactor

---
+++ internal/accounting/reports/report/domain/interactors/update_report_interactor.go
@@ -0,0 +1,33 @@
+package interactors
+
+import (
+	"github.com/zhuravlevma/golang-ddd-architecture/internal/accounting/reports/report/domain/entities"
+	"github.com/zhuravlevma/golang-ddd-architecture/internal/accounting/reports/report/domain/ports/in"
+	"github.com/zhuravlevma/golang-ddd-architecture/internal/accounting/reports/report/domain/ports/out"
+)
+
+type UpdateReportInteractor struct {
+	findReportByIdPort out.FindReportByIdOutPort
+	saveReportPort out.SaveReportOutPort
+}
+
+func NewUpdateReportInteractor(
+	findReportByIdPort out.FindReportByIdOutPort,
+	saveReportPort out.SaveReportOutPort,
+) UpdateReportInteractor {
+	return UpdateReportInteractor{findReportByIdPort, saveReportPort}
+}
+
+func (s *UpdateReportInteractor) execute(updateReportParams in.UpdateReportParams) (*entities.ReportEntity, error) {
+	report, err := s.findReportByIdPort.FindReportById(updateReportParams.ReportId)
+
+	if (err != nil) {
+		return nil, err
+	}
+
+	if (updateReportParams.IsValid != nil) {
+		report.UpdateReportStatus(*updateReportParams.IsValid)
+	}
+
+	return s.saveReportPort.Save(report)
+}
--- internal/accounting/reports/report/domain/ports/out/find_report_by_id_out_port.go
+++ internal/accounting/reports/report/domain/ports/out/find_report_by_id_out_port.go
@@ -5,5 +5,5 @@ import (
 	"github.com/zhuravlevma/golang-ddd-architecture/internal/accounting/reports/report/domain/entities"
 )
 type FindReportByIdOutPort interface {
-	findReportById(reportId uuid.UUID) (*entities.ReportEntity, error)
+	FindReportById(reportId uuid.UUID) (*entities.ReportEntity, error)
 }
