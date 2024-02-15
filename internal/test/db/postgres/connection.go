9885a584c7ab46de4d68b22d12c451f622367d28
zhuravlevma <monge.gaspard.1746@gmail.com>
2024-02-15 23:06:50 +0300

    fix: remove test folder

--- internal/test/db/postgres/connection.go
+++
@@ -1,9 +0,0 @@
-package postgres
-
-import (
-	"github.com/jinzhu/gorm"
-)
-
-func NewConnection() (*gorm.DB, error) {
-	return gorm.Open("postgres", "your_connection_string_here")
-}
--- internal/test/entities/dtos/create_product_dto.go
+++
@@ -1,6 +0,0 @@
-package dtos
-
-type CreateProductDto struct {
-	Name     string  `json:"Name"`
-	Price    float64 `json:"Price"`
-}
