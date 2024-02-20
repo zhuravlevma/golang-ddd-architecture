e664e3643411e77b3b4c4f4346558b384f20e444
zhuravlevma <monge.gaspard.1746@gmail.com>
2024-02-20 22:19:35 +0300

    fix: add env and config

--- internal/__config__/config.go
+++ internal/__config__/config.go
@@ -35,7 +35,6 @@ func New() *Config {
     }
 }
 
-// Simple helper function to read an environment or return a default value
 func getEnv(key string, defaultVal string) string {
     if value, exists := os.LookupEnv(key); exists {
 	return value
@@ -44,7 +43,6 @@ func getEnv(key string, defaultVal string) string {
     return defaultVal
 }
 
-// Simple helper function to read an environment variable into integer or return a default value
 func getEnvAsInt(name string, defaultVal int) int {
     valueStr := getEnv(name, "")
     if value, err := strconv.Atoi(valueStr); err == nil {
@@ -54,7 +52,6 @@ func getEnvAsInt(name string, defaultVal int) int {
     return defaultVal
 }
 
-// Helper to read an environment variable into a bool or return default value
 func getEnvAsBool(name string, defaultVal bool) bool {
     valStr := getEnv(name, "")
     if val, err := strconv.ParseBool(valStr); err == nil {
@@ -64,7 +61,6 @@ func getEnvAsBool(name string, defaultVal bool) bool {
     return defaultVal
 }
 
-// Helper to read an environment variable into a string slice or return default value
 func getEnvAsSlice(name string, defaultVal []string, sep string) []string {
     valStr := getEnv(name, "")
 
